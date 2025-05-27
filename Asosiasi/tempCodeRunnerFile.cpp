d3e88b6a5ac5bbcf901caba7d2d5e8ce39bd0426
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-27 19:50:11 +0700

    menambahkan program dokter ke menu utama

--- Asosiasi/tempCodeRunnerFile.cpp
+++ Asosiasi/tempCodeRunnerFile.cpp
@@ -1,15 +1,26 @@
-class dokter {
-    public :
-        string nama;
-        vector<pasien*> daftar_pasien;
+int main() {
+     dokter* varDokter1 = new dokter("dr.Budi");
+     dokter* varDokter2 = new dokter("dr.Tono");
+     pasien* varPasien1 = new pasien ("andi");
+     pasien* varPasien2 = new pasien ("meilan");
 
-        dokter (string pNama) : nama (pNama) {
-            cout << "Dokter \"" << nama << "\" ada\n";
-        }
-        ~dokter() {
-            cout << "Dokter \"" << nama
-            << "\" tidak ada\n";
-        }
-        void tambahPasien(pasien*);
-        void cetakPasien();
-};
+     varDokter1->tambahPasien(varPasien1);
+     varDokter1->tambahPasien(varPasien2);
+     varDokter2->tambahPasien(varPasien1);
+
+     varPasien1->tambahDokter(varDokter1);
+     varPasien2->tambahDokter(varDokter1);
+     varPasien1->tambahDokter(varDokter2);
+
+     varDokter1->cetakPasien();
+     varDokter2->cetakPasien();
+     varPasien1->cetakDokter();
+     varPasien2->cetakDokter();
+
+     delete varPasien1;
+     delete varPasien2;
+     delete varDokter1;
+     delete varDokter2;
+
+     return 0;
+}
