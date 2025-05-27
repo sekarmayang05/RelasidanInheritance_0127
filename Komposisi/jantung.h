a8b6441a3046fcb354d2104915271463c56bf851
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-27 20:26:32 +0700

    menambahkan class jantung dengan komposisi

---
+++ Komposisi/jantung.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+    public :
+        jantung () {
+            cout << "jantung dihidupkan\n";
+        }
+        ~jantung() {
+            cout << "jantung dimatikan\n";
+        }
+};
+#endif
